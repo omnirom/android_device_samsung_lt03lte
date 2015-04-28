fc91fca30ea1e5b18952253bb479bda0cd5212a4
Jake Whatley <jacob.whatley91@gmail.com>
2015-04-28 18:28:00 -0400

    lt03lte: build full_base
    - Stock does not have Dialer
    
    Change-Id: I3fbdf20302348d7de0e7fb6f2fc48b36982f45df

--- omni_lt03lte.mk
+++ omni_lt03lte.mk
@@ -22,7 +22,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from lt03lte device
 $(call inherit-product, device/samsung/lt03lte/device.mk)
 
